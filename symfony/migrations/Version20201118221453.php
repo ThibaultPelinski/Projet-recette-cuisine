5c5fe5be1457a7d47a67666a6cc497ca8cd3e931
p.thibault10 <thibault@spliiit.com>
2020-11-19 12:38:39 +0100

    added src & template

---
+++ symfony/migrations/Version20201118221453.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201118221453 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE create_recette (id INT AUTO_INCREMENT NOT NULL, titre_recette VARCHAR(255) NOT NULL, presentation VARCHAR(255) NOT NULL, nb_personnes INT NOT NULL, difficulte INT NOT NULL, ingredients VARCHAR(255) NOT NULL, instruments VARCHAR(255) NOT NULL, recette VARCHAR(255) NOT NULL, temps_cuisson TIME NOT NULL, temps_preparation TIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE create_recette');
+    }
+}
